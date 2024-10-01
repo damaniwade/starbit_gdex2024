49f79c3f85055a30dd6a3a657ca823bbbc887045
damaniwade <74627292+damaniwade@users.noreply.github.com>
2024-10-01 10:19:40 -0400

    Added player controls script and implemented basic left and right movement.

---
+++ Scenes/player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(100, 500)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		position += Vector2(150, 0) * delta
+	if Input.is_action_pressed("ui_left"):
+		position -= Vector2(150, 0) * delta
