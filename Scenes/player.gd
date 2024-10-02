80680a63b59d146ff44c52b207efdda83ee1f644
damaniwade <74627292+damaniwade@users.noreply.github.com>
2024-10-02 00:47:50 -0400

    Basic player jump control and gravity implemented. As well as collision detection for ground movement.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+#const GRAVITY  = 200.0
+
+#var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		position += Vector2(150, 0) * delta
-	if Input.is_action_pressed("ui_left"):
-		position -= Vector2(150, 0) * delta
+	#if Input.is_action_pressed("ui_right"):
+		#position += Vector2(150, 0) * delta
+	#if Input.is_action_pressed("ui_left"):
+		#position -= Vector2(150, 0) * delta
+	pass
