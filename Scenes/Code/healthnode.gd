e96c13176466f03a0c481b665863786732b7561e
ZenAshburn <ashroahrig@gmail.com>
2024-10-06 22:21:48 -0400

    fixed damage to enemies
    little and boss took 0 damage

--- Scenes/Code/healthnode.gd
+++ Scenes/Code/healthnode.gd
@@ -4,9 +4,5 @@ extends Node
 @export var HP: int = 5
 var p = get_parent()
 
-func _process(_delta):
-	if HP <= 0:
-		if p.is_in_group("Player"):
-			get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
 
 	
